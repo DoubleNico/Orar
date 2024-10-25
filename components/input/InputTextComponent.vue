0e0bd502313e22cf175f738edcd375a30de3f9fb
DoubleNico <tomescuvlad123@gmail.com>
2024-10-25 18:54:28 +0300

    Added a text input component to check for text input

---
+++ components/input/InputTextComponent.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <label :for="id" class="block text-sm font-medium text-modal">
+      {{ label }}
+    </label>
+    <input
+      :id="id"
+      v-model="inputValue"
+      type="text"
+      :placeholder="placeholder"
+      :maxlength="maxlength"
+      :pattern="pattern"
+      class="mt-1 block w-full p-2 border rounded-md text-input-modal"
+      :class="{
+        'border-invalid': hasRestrictedChars,
+        'outline-valid': !hasRestrictedChars,
+        'outline-invalid': hasRestrictedChars,
+      }"
+      @keyup="check"
+    />
+    <p v-if="hasRestrictedChars" class="text-red-500 text-sm mt-1">
+      Invalid characters detected
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+  label: {
+    type: String,
+    default: 'Text Input',
+  },
+  placeholder: {
+    type: String,
+    default: 'Enter a Text',
+  },
+  id: {
+    type: String,
+    default: 'text-input',
+  },
+  maxlength: {
+    type: String,
+    default: '100',
+  },
+  pattern: {
+    type: String,
+    default: '',
+  },
+})
+
+const emit = defineEmits(['update:modelValue', 'check'])
+
+const inputValue = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value),
+})
+
+const hasRestrictedChars = ref(false)
+
+function check() {
+  if (inputValue.value.length < 2) {
+    const restrictedChars = ['d', '1', '0', '+']
+    hasRestrictedChars.value = restrictedChars.some((char) =>
+      inputValue.value.includes(char),
+    )
+  } else {
+    hasRestrictedChars.value = false
+  }
+  emit('check', !hasRestrictedChars.value)
+}
+</script>
