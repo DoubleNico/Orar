5510d4c72ff964d12cb3bfae968a03ffd1984e87
DoubleNico <tomescuvlad123@gmail.com>
2024-12-13 22:34:46 +0200

    Removed unusued imports on columnsOperations

--- scripts/backend/columnsOperations.ts
+++ scripts/backend/columnsOperations.ts
@@ -5,15 +5,13 @@ export async function addColumnsToDatabase(
   columnsCount: number,
   config: RuntimeConfig,
 ) {
-  try {
-    await $fetch(
-      `${config.public.backend}/${config.public.api_link}/${config.public.schedule_link}/${scheduleId}/addColumn`,
-      {
-        method: 'POST',
-        body: JSON.stringify({ id: scheduleId, columns: columnsCount }),
-      },
-    )
-  } catch (error) {
-    throw new Error('Failed to add column')
-  }
+  await $fetch(
+    `${config.public.backend}/${config.public.api_link}/${config.public.schedule_link}/${scheduleId}/addColumn`,
+    {
+      method: 'POST',
+      body: JSON.stringify({ id: scheduleId, columns: columnsCount }),
+    },
+  ).catch((error) => {
+    throw new Error("Couldn't add columns to the database: " + error.data)
+  })
 }
