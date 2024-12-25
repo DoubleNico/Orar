de480b0bf276610705bd929f40f7a4ca4da13d4e
DoubleNico <tomescuvlad123@gmail.com>
2024-12-25 22:56:20 +0200

    Added the auth middleware to /middleware folder

---
+++ middleware/auth.ts
@@ -0,0 +1,21 @@
+import { parseExpiration } from '~/scripts/auth/utils/parseExpiration'
+
+export default defineNuxtRouteMiddleware(async (to) => {
+  const config = useRuntimeConfig()
+  const { data, error } = await useFetch('/api/checkTokens')
+  if (error.value || !data.value?.success) {
+    if (to.path !== '/login') return navigateTo('/login')
+    return
+  }
+  if (data.value?.newAccessToken) {
+    const cookie = useCookie('id', {
+      httpOnly: config.public.httpOnly,
+      secure: config.public.isProduction,
+      sameSite: config.public.sameSite as 'lax' | 'strict' | 'none',
+      path: config.public.auth_path,
+      domain: config.public.isProduction ? `.${config.public.domain}` : '',
+      maxAge: parseExpiration(config.public.accessTokenExpiration),
+    })
+    cookie.value = data.value.newAccessToken
+  }
+})
