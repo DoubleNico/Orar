37de7319bf9617e22e39a1b43587b9d7d95fce86
DoubleNico <tomescuvlad123@gmail.com>
2024-12-25 22:57:52 +0200

    Added a button component for the login

---
+++ components/LoginButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      @click="navigateToLogin"
+    >
+      Login
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function navigateToLogin() {
+  router.push('/login')
+}
+</script>
