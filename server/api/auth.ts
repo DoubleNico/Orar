9fc09225c3bd48ebec5cd19595ecd2b2dc1a8534
DoubleNico <tomescuvlad123@gmail.com>
2025-02-17 17:20:49 +0200

    Remade auth.ts for checking passwords

--- server/api/auth.ts
+++ server/api/auth.ts
@@ -1,16 +1,37 @@
+import { z } from 'zod'
 import { AuthInit } from '~/scripts/auth/authInit'
 import { NuxtResponseAdapter } from '~/scripts/auth/responses/NuxtResponseAdapter'
+import { checkPassword } from '~/scripts/backend/usersOperations'
+
+const userSchema = z.object({
+  username: z.string(),
+  password: z.string().min(2),
+})
 
 export default defineEventHandler(async (event) => {
+  const result = await readValidatedBody(event, (body) =>
+    userSchema.safeParse(body),
+  )
+  if (!result.success) throw result.error.issues
   const oldRefreshToken = getCookie(event, 'rid')
   const authInit = new AuthInit()
+  const config = useRuntimeConfig()
+
+  const user = await checkPassword(
+    config,
+    result.data.username,
+    result.data.password,
+  )
+  if (!user) {
+    return { success: false, message: 'Invalid credentials' }
+  }
 
   try {
     const responseAdapter = new NuxtResponseAdapter(event)
 
     await authInit.authUtils.sendAuthCookies(
       responseAdapter,
-      '123',
+      user.id,
       oldRefreshToken,
     )
 
