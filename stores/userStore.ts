3b4e8dd878ec5d7b90bd5815a4b855a74eec11e2
DoubleNico <tomescuvlad123@gmail.com>
2025-02-17 17:38:38 +0200

    Added a pinia store for userId

---
+++ stores/userStore.ts
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({ userId: null as string | null }),
+  actions: {
+    setUserId(id: string) {
+      this.userId = id
+    },
+  },
+})
