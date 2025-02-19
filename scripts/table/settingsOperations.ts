98c6b9a6d95dc83a2090bcbff31f7a40e4ecd0fe
DoubleNico <tomescuvlad123@gmail.com>
2025-02-19 22:40:37 +0200

    Add saveSettings function for managing cell settings in the table

---
+++ scripts/table/settingsOperations.ts
@@ -0,0 +1,31 @@
+import type { RuntimeConfig } from 'nuxt/schema'
+import { saveSettingsToDatabase } from '../backend/courseOperations'
+import type { CellSettings } from './types/CellSettings'
+import {
+  cellAlignment,
+  cellAlignments,
+  fontColor,
+  fontSize,
+  modalColumn,
+  modalRow,
+  selectedColor,
+  wrapText,
+} from './state'
+import { isModalOpen, isSettingsModalOpen } from './modal/modalState'
+
+export async function saveSettings(
+  settings: CellSettings,
+  scheduleId: string,
+  config: RuntimeConfig,
+) {
+  await saveSettingsToDatabase(settings, scheduleId, config).finally(() => {
+    cellAlignments.set(`${modalRow.value}-${modalColumn.value}`, settings)
+    selectedColor.value = settings.color
+    cellAlignment.value = settings.alignment
+    wrapText.value = settings.wrapText
+    fontSize.value = settings.fontSize
+    fontColor.value = settings.fontColor
+    isSettingsModalOpen.value = false
+    isModalOpen.value = true
+  })
+}
