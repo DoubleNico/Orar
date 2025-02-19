623bd76a7ea2f7872b7e01156bffd3712f86799f
DoubleNico <tomescuvlad123@gmail.com>
2025-02-19 22:40:47 +0200

    Add modal state management for settings in the table

---
+++ scripts/table/modal/modalState.ts
@@ -0,0 +1,18 @@
+import { cellName } from '../state'
+
+const isModalOpen = ref(false)
+const isSettingsModalOpen = ref(false)
+
+function openSettings(initialCellName: string) {
+  cellName.value = initialCellName
+  isModalOpen.value = false
+  isSettingsModalOpen.value = true
+}
+
+function closeSettingsModal(initialCellName: string) {
+  cellName.value = initialCellName
+  isSettingsModalOpen.value = false
+  isModalOpen.value = true
+}
+
+export { isModalOpen, isSettingsModalOpen, openSettings, closeSettingsModal }
